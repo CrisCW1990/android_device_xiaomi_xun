b9bd5feb9e3bf6be867a167fccdfe40a0a8b25dc
CrisCW1990 <38779980+CrisCW1990@users.noreply.github.com>
2023-11-18 18:21:05 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_xun-user
-add_lunch_combo omni_xun-userdebug
-add_lunch_combo omni_xun-eng
+add_lunch_combo twrp_xun-user
+add_lunch_combo twrp_xun-userdebug
+add_lunch_combo twrp_xun-eng
